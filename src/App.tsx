6b604f22853bd4aa4dde5295bad9aa69c55cf601
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-07-08 00:38:53 +0600

    added store

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h2>Hi</h2>
-    </>
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="flex gap-10 border border-purple-300 rounded bg-slate-50 p-20">
+        <button className="px-3 py-2 rounded bg-green-500 text-xl font-semibold text-white">
+          Increment
+        </button>
+        <p className="text-3xl">0</p>
+        <button className="px-3 py-2 rounded bg-red-500 text-xl font-semibold text-white">
+          Decrement
+        </button>
+      </div>
+    </div>
   );
 }
 
