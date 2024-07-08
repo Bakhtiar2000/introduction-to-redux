8869cb0c111a934adfcaf7e671f2a3b99d854434
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-07-09 00:39:13 +0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./redux/features/CounterSlice";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "./redux/features/CounterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const { count } = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <div className="flex justify-center items-center h-screen w-full">
       <div className="flex gap-10 border border-purple-300 rounded bg-slate-50 p-20">
@@ -12,6 +16,12 @@ function App() {
           className="px-3 py-2 rounded bg-green-500 text-xl font-semibold text-white"
         >
           Increment
+        </button>
+        <button
+          onClick={() => dispatch(incrementByValue(8))}
+          className="px-3 py-2 rounded bg-green-500 text-xl font-semibold text-white"
+        >
+          Increment By Value
         </button>
         <p className="text-3xl">{count}</p>
         <button
